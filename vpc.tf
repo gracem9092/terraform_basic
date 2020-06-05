a73ec1a1393659ef9ffdb577abcc7a805c823e83
Grace Matshiye <gracem@thoughtworks.com>
2020-06-05 11:37:54 +0200

    Create  NAT getway and a route table for private subnets

--- vpc.tf
+++ vpc.tf
@@ -102,3 +102,41 @@ resource "aws_route_table_association" "public_3" {
   subnet_id      = aws_subnet.public_3.id
   route_table_id = aws_route_table.route-public.id
 }
+
+# nat gateway && Route table for private subnet
+resource "aws_eip" "nat" {
+  vpc = true
+}
+resource "aws_nat_gateway" "nat_gw" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public_1.id
+  depends_on    = ["aws_internet_gateway.igw_demo"]
+
+  tags = {
+    Name = "nat-gw-demo"
+  }
+}
+resource "aws_route_table" "route-private" {
+  vpc_id = aws_vpc.vpc_demo.id
+
+   route {
+     cidr_block = "10.0.0.0/0"
+     gateway_id = aws_nat_gateway.nat_gw.id
+   }
+
+   tags = {
+     Name = "private-route-table-demo"
+   }
+ }
+resource "aws_route_table_association" "private_1" {
+  subnet_id      = aws_subnet.private_1.id
+  route_table_id = aws_route_table.route-private.id
+}
+resource "aws_route_table_association" "private_2" {
+  subnet_id      = aws_subnet.private_2.id
+  route_table_id = aws_route_table.route-private.id
+}
+resource "aws_route_table_association" "private_3" {
+  subnet_id      = aws_subnet.private_3.id
+  route_table_id = aws_route_table.route-private.id
+}
