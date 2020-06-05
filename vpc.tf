f0cc7545a57df90aa9a02d124a343358e64a5701
Grace Matshiye <gracem@thoughtworks.com>
2020-06-05 11:31:44 +0200

    Creates public and private subnets

--- vpc.tf
+++ vpc.tf
@@ -11,3 +11,61 @@ resource "aws_vpc" "vpc_demo" {
     Name = var.tags
   }
 }
+
+#Public subnets
+resource "aws_subnet" "public_1" {
+  vpc_id     = aws_vpc.vpc_demo.id
+  map_public_ip_on_launch = true
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "public_1-demo"
+  }
+}
+resource "aws_subnet" "public_2" {
+  vpc_id     = aws_vpc.vpc_demo.id
+  map_public_ip_on_launch = true
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "public_2-demo"
+  }
+}
+resource "aws_subnet" "public_3" {
+  vpc_id     = aws_vpc.vpc_demo.id
+  map_public_ip_on_launch = true
+  cidr_block = "10.0.3.0/24"
+
+  tags = {
+    Name = "public_3-demo"
+  }
+}
+
+#Private subnets
+resource "aws_subnet" "private_1" {
+  vpc_id     = aws_vpc.vpc_demo.id
+  map_public_ip_on_launch = false
+  cidr_block = "10.0.4.0/24"
+
+  tags = {
+    Name = "private_1-demo"
+  }
+}
+resource "aws_subnet" "private_2" {
+  vpc_id     = aws_vpc.vpc_demo.id
+  map_public_ip_on_launch = false
+  cidr_block = "10.0.5.0/24"
+
+  tags = {
+    Name = "private_2-demo"
+  }
+}
+resource "aws_subnet" "private_3" {
+  vpc_id     = aws_vpc.vpc_demo.id
+  map_public_ip_on_launch = false
+  cidr_block = "10.0.6.0/24"
+
+  tags = {
+    Name = "private_3-demo"
+  }
+}
