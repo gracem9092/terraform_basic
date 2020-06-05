0bedd8f3ab954ebb28fc51cca687a47213d39186
Grace Matshiye <gracem@thoughtworks.com>
2020-06-05 11:30:29 +0200

    Creates a custome VPC

--- variable.tf
+++ variable.tf
@@ -3,3 +3,28 @@ variable "secret_key" {}
 variable "region" {
   default = "us-east-1"
 }
+
+
+variable "cidr_block" {
+  default = "10.0.0.0/16"
+}
+variable "instance_tenancy" {
+  default = "default"
+}
+variable "enable_dns_hostnames" {
+  type = bool
+  default = true
+}
+variable "enable_dns_support" {
+  type = bool
+  default = true
+}
+variable "enable_classiclink" {
+  type = bool
+  default = false
+}
+
+variable "tags" {
+  type = string
+  default = "vpc-custom-demo"
+}
---
+++ vpc.tf
@@ -0,0 +1,13 @@
+# VPC
+resource "aws_vpc" "vpc_demo" {
+  cidr_block       = var.cidr_block
+  instance_tenancy = var.instance_tenancy
+  enable_dns_hostnames = var.enable_dns_hostnames
+  enable_dns_support = var.enable_dns_support
+  enable_classiclink = var.enable_classiclink
+
+
+  tags = {
+    Name = var.tags
+  }
+}
